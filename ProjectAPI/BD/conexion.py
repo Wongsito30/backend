29600784b76def92c0ee91c36d1c58b22583f63c
Wongsito30 <sahirwong9803@gmail.com>
2024-01-01 23:20:16 -0700

    dios?

--- ProjectAPI/BD/conexion.py
+++ ProjectAPI/BD/conexion.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-databaseURL = "mysql+mysqlconnector://root:2gED4feaeAE2HgFFFB1a4Ea2C6fHgd3D@viaduct.proxy.rlwy.net:52528/tiendaderopa"
+databaseURL = "mysql+mysqlconnector://root:BfhGc22a4dDdfB1-f4DhDabc4GG-EFGF@viaduct.proxy.rlwy.net:51123/tiendaderopa"
 
 engine = create_engine(databaseURL)
 
